9c1734b3c14520e5348c5ea0d36125c6582be7b7
Brenden Smith <brenden@brendensmith.net>
2022-03-21 12:53:57 -0500

    update for event listener POST test

--- new/raemis/cgi-bin/events.py
+++ new/raemis/cgi-bin/events.py
@@ -7,12 +7,19 @@ import cgitb
 
 cgitb.enable()
 
+form = cgi.FieldStorage()
+
+
 cgi.print_environ()
 cgi.print_arguments()
 cgi.print_directory()
+cgi.print_environ()
+cgi.print_form(dict(form))
 
 if sys.argv:
     pprint.pprint(sys.argv)
 
+for param in os.environ.keys():
+    print("<b>%20s</b>: %s<\br>" % (param, os.environ[param]))
 
 print("running")
