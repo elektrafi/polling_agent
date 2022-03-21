41fdc8596d90e0dbfb6c5e737ffb55f5e0e27f0c
Brenden Smith <brenden@brendensmith.net>
2022-03-21 12:37:26 -0500

    update for event listener POST test

--- new/raemis/cgi-bin/events.py
+++ new/raemis/cgi-bin/events.py
@@ -2,6 +2,14 @@
 import sys
 import os
 import pprint
+import cgi
+import cgitb
+
+cgitb.enable()
+
+cgi.print_environ()
+cgi.print_arguments()
+cgi.print_directory()
 
 if sys.argv:
     pprint.pprint(sys.argv)
