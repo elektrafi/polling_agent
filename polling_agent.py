edc51dcdef57cc32b01822b371fae8038bd9b6be
Brenden Smith <brenden@brendensmith.net>
2022-03-31 02:40:04 -0500

    cleanup

--- polling_agent.py
+++ polling_agent.py
@@ -12,15 +12,12 @@ from .sonar.ip_allocation import Allocator as _Allocator
 from .snmp import Session as _Session
 from .model.atoms import Item as _Item, Manufacturer as _Manufacturer
 from .model.structures import MergeSet as _MergeSet
-<<<<<<< Updated upstream
-=======
 from multiprocessing.pool import Pool as _Pool
 from multiprocessing import Process as _Process
 import asyncio as _asyncio
 from asyncio import run as _run
 
 _exec = _Pool()
->>>>>>> Stashed changes
 
 
 class PollingAgent:
@@ -45,50 +42,6 @@ class PollingAgent:
         # self.allocator.start_loop()
         self._startup()
 
-<<<<<<< Updated upstream
-    def _startup(self):
-        inv = self._pipeline.start_fn(
-            self._sonar.get_all_clients_and_assigned_inventory
-        )
-        inv.add_done_callback(self._assign_cb)
-        subs = self._pipeline.start_fn_after_future(inv, self._raemis.get_subscribers)
-        subs.add_done_callback(self._assign_cb)
-        ips = self._pipeline.start_fn_after_future(subs, self._raemis.get_data_sessions)
-        ips.add_done_callback(self._assign_cb)
-        snmp = self._pipeline.start_fn_after_future(ips, self._update_snmp_info)
-        self._pipeline.start_fn_after_future(
-            snmp, lambda: self._logger.info("Finished startup process")
-        )
-
-    def _update_snmp_info(self) -> list[_Item]:
-        def snmp_info(i: _Item):
-            snmp = _Session(str(i.ipv4))
-            i = snmp.get_item_values()
-            self._inventory.add(i)
-            return i
-
-        return list(self._pipeline.map(snmp_info, self._inventory))
-
-    def _assign_cb(self, l: _Future[list[_Item]]) -> None:
-        while not l.done():
-            pass
-        if l.exception():
-            self._logger.error(
-                f"raemis encountered error getting subscribers: {l.exception()}"
-            )
-        result = l.result()
-        if result is None:
-            self._logger.error("raemis returned no subscribers")
-
-        def fn(i: _Item) -> None:
-            if i:
-                try:
-                    self._inventory.add(i)
-                except:
-                    self._logger.exception(f"exception updating {i}", stack_info=True)
-
-        list(self._pipeline.map(fn, result))
-=======
     def _starter(self):
         self._logger.info("starting initilization sync")
         inv = _run(
@@ -111,4 +64,3 @@ class PollingAgent:
                 _Session.get_item_values()
 
         return tasks
->>>>>>> Stashed changes
