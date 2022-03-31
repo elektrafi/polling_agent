473bf7897ee3cb9dcd9e965cb6239d7f94e8f06f
Brenden Smith <brenden@brendensmith.net>
2022-03-31 02:35:42 -0500

    update from stash

--- polling_agent.py
+++ polling_agent.py
@@ -10,8 +10,17 @@ from .raemis.api_connection import Raemis as _Raemis
 from .raemis.event_listener import Listener as _Listener
 from .sonar.ip_allocation import Allocator as _Allocator
 from .snmp import Session as _Session
-from .model.atoms import Item as _Item
+from .model.atoms import Item as _Item, Manufacturer as _Manufacturer
 from .model.structures import MergeSet as _MergeSet
+<<<<<<< Updated upstream
+=======
+from multiprocessing.pool import Pool as _Pool
+from multiprocessing import Process as _Process
+import asyncio as _asyncio
+from asyncio import run as _run
+
+_exec = _Pool()
+>>>>>>> Stashed changes
 
 
 class PollingAgent:
@@ -36,6 +45,7 @@ class PollingAgent:
         # self.allocator.start_loop()
         self._startup()
 
+<<<<<<< Updated upstream
     def _startup(self):
         inv = self._pipeline.start_fn(
             self._sonar.get_all_clients_and_assigned_inventory
@@ -78,3 +88,27 @@ class PollingAgent:
                     self._logger.exception(f"exception updating {i}", stack_info=True)
 
         list(self._pipeline.map(fn, result))
+=======
+    def _starter(self):
+        self._logger.info("starting initilization sync")
+        inv = _run(
+            self._sonar.execute(self._sonar.get_all_clients_and_assigned_inventory)
+        )
+        inv = list(_exec.map(self._inventory.add, inv, chunksize=125))
+        subs = _run(self._raemis.get_subscribers())
+        subs = list(_exec.map(self._inventory.add, subs, chunksize=150))
+        ips = _run(self._raemis.get_data_sessions())
+        ips = list(_exec.map(self._inventory.add, ips))
+        snmp = self._update_snmp_info()
+        snmp = self._pool.map(self._inventory.add, snmp)
+        self._logger.info("Finished startup process")
+
+    def _update_snmp_info(self) -> _Iterable[_Item]:
+        self._logger.info("getting SNMP info from devices")
+        tasks: list[_Item] = []
+        for item in self._inventory:
+            if item.manufacturer != _Manufacturer.BAICELLS:
+                _Session.get_item_values()
+
+        return tasks
+>>>>>>> Stashed changes
